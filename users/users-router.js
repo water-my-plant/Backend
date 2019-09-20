7a95c6fb5939e4f8057c5587e8917bcf3bd052d5
MelvinPeepers <peepersm@gmail.com>
2019-09-19 21:45:29 -0400

    setup find user by id

--- users/users-router.js
+++ users/users-router.js
@@ -11,4 +11,22 @@ router.get("/", (req, res) => {
 });
 // GET localhost:5000/api/users tested in Postman
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Users.findById(id)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find user with gviven ID." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Failed to get user" });
+    });
+});
+// GET localhost:5000/api/users/3 tested in Postman
+
 module.exports = router;
