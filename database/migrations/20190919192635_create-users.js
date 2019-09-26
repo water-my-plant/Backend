b65a19a17d7896f31d3fa14dc05829d1abfe9258
MelvinPeepers <peepersm@gmail.com>
2019-09-25 21:29:27 -0400

    made changes to the schema and removed two seeds

--- database/migrations/20190919192635_create-users.js
+++ database/migrations/20190919192635_create-users.js
@@ -1,61 +1,60 @@
 exports.up = function(knex) {
-  return knex.schema
-    .createTable("users", tbl => {
-      tbl.increments();
-      tbl.string("fullname", 128).notNullable();
-      tbl
-        .string("username", 128)
-        .notNullable()
-        .unique();
-      tbl.string("password", 128).notNullable();
-      tbl.string("phonenumber", 11);
-    })
-    .createTable("water", tbl => {
-      tbl.increments();
-      tbl.string("water_schedule", 256);
-    })
-    .createTable("plants", tbl => {
-      tbl.increments();
-      tbl.string("plant_name", 256).notNullable();
-      tbl.string("plant_species", 256).notNullable();
-      // foreign key setup using knex
-      tbl
-        .integer("water_id")
-        .unsigned()
-        .references("id")
-        // this table must exist
-        .inTable("water")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
-    })
-    .createTable("water_plants", tbl => {
-      tbl
-        .integer("water_id")
-        .unsigned()
-        .notNullable()
-        .references("id")
-        // this table must exist
-        .inTable("water")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
-      tbl
-        .integer("plants_id")
-        .unsigned()
-        .notNullable()
-        .references("id")
-        // this table must exist
-        .inTable("plants")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
-      tbl.primary(["water_id", "plants_id"]);
-    });
+  return (
+    knex.schema
+      .createTable("users", tbl => {
+        tbl.increments();
+        tbl.string("fullname", 128).notNullable();
+        tbl
+          .string("username", 128)
+          .notNullable()
+          .unique();
+        tbl.string("password", 128).notNullable();
+        tbl.string("phonenumber", 11);
+      })
+      // .createTable("water", tbl => {
+      //   tbl.increments();
+      //   tbl.string("water_schedule", 256);
+      // })
+      .createTable("plants", tbl => {
+        tbl.increments();
+        tbl.string("plant_name", 256).notNullable();
+        tbl.string("plant_species", 256).notNullable();
+        tbl.string("water_schedule", 512).notNullable();
+        // foreign key setup using knex
+        // tbl
+        //   .integer("water_id")
+        //   .unsigned()
+        //   .references("id")
+        //   // this table must exist
+        //   .inTable("water")
+        //   .onDelete("CASCADE")
+        //   .onUpdate("CASCADE");
+        // })
+        // .createTable("water_plants", tbl => {
+        //   tbl
+        //     .integer("water_id")
+        //     .unsigned()
+        //     .notNullable()
+        //     .references("id")
+        //     // this table must exist
+        //     .inTable("water")
+        //     .onDelete("CASCADE")
+        //     .onUpdate("CASCADE");
+        //   tbl
+        //     .integer("plants_id")
+        //     .unsigned()
+        //     .notNullable()
+        //     .references("id")
+        //     // this table must exist
+        //     .inTable("plants")
+        //     .onDelete("CASCADE")
+        //     .onUpdate("CASCADE");
+        //   tbl.primary(["water_id", "plants_id"]);
+      })
+  );
 };
 
 exports.down = function(knex) {
   // reverse order of creation
-  return knex.schema
-    .dropTableIfExists("water_plants")
-    .dropTableIfExists("plants")
-    .dropTableIfExists("water")
-    .dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("plants").dropTableIfExists("users");
 };
--- database/seeds/03-plants.js
+++ database/seeds/03-plants.js
@@ -8,47 +8,47 @@ exports.seed = function(knex) {
         {
           plant_name: "Abutilon",
           plant_species: "Flowering Maple",
-          water_id: 1
+          water_schedule: "Water in 1 day"
         },
         {
           plant_name: "Summer Bloom",
           plant_species: "Ageratum",
-          water_id: 2
+          water_schedule: "Water in 2 day"
         },
         {
           plant_name: "Aloe Vera",
           plant_species: "Aloe barbadensis",
-          water_id: 3
+          water_schedule: "Water in 3 day"
         },
         {
           plant_name: "Barberton Daisy",
           plant_species: "Gebera Jamesonii",
-          water_id: 4
+          water_schedule: "Water in 4 day"
         },
         {
           plant_name: "Bird of Paradise",
           plant_species: "Strelitzia Reginae",
-          water_id: 5
+          water_schedule: "Water in 5 day"
         },
         {
           plant_name: "Busy Lizzie",
           plant_species: "Impatiens Walleriana",
-          water_id: 6
+          water_schedule: "Water in 6 day"
         },
         {
           plant_name: "Calla lily",
           plant_species: "Zantedeschia aethiopica",
-          water_id: 7
+          water_schedule: "Water in 7 day"
         },
         {
           plant_name: "Eternal Flame",
           plant_species: "Calathea Crocata",
-          water_id: 8
+          water_schedule: "Water in 8 day"
         },
         {
           plant_name: "Lollipop",
           plant_species: "Pachystachys Lutea",
-          water_id: 9
+          water_schedule: "Water in 9 day"
         }
       ]);
     });
